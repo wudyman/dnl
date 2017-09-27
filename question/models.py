2f00c98fb51e59a098c75bf5236160c12bf666c0
dongxw <wudy.dong@163.com>
2017-09-27 16:50:12 +0800

    add the question app, create the model Question

--- question/models.py
+++ question/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+	question_title=models.CharField(max_length=50)
+	question_detail=models.CharField(max_length=200)
+	pub_date=models.DateTimeField('date published')
+	quizzer=models.ForeignKey(User,on_delete=models.CASCADE)
+	def __str__(self):
+		return self.question_title
