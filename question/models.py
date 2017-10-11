bf0994c20e85f1124aeb9e85acbebd782350f63d
dongxw <wudy.dong@163.com>
2017-10-11 15:48:34 +0800

    update

--- question/models.py
+++ question/models.py
@@ -4,17 +4,28 @@ import django.utils.timezone as timezone
 
 # Create your models here.
 class Question(models.Model):
-	title=models.CharField(max_length=50)
-	detail=models.CharField(max_length=200)
-	pub_date=models.DateTimeField('date published',default=timezone.now)
-	quizzer=models.ForeignKey(User,on_delete=models.CASCADE)
-	def __str__(self):
-		return self.title
+    title=models.CharField(max_length=50)
+    detail=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published',default=timezone.now)
+    quizzer=models.ForeignKey(User,on_delete=models.CASCADE)
+    def __str__(self):
+        return self.title
 
 class Topic(models.Model):
-	name=models.CharField(max_length=50)
-	detail=models.CharField(max_length=200)
-	question=models.ManyToManyField(Question)
-	def __str__(self):
-	    return self.name
+    name=models.CharField(max_length=50)
+    detail=models.CharField(max_length=200)
+    question=models.ManyToManyField(Question)
+    pub_date=models.DateTimeField('date published',default=timezone.now)
+    def __str__(self):
+        return self.name
 
+class Answer(models.Model):
+    question=models.ForeignKey(Question,on_delete=models.CASCADE)
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.CharField(max_length=2000)
+    pub_date=models.DateTimeField('date published',default=timezone.now)
+
+class Comment(models.Model):
+    author=models.ForeignKey(User,on_delete=models.CASCADE)
+    content=models.CharField(max_length=200)
+    pub_date=models.DateTimeField('date published',default=timezone.now)
--- question/urls.py
+++ question/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.IndexView.as_view(),name='question_index')
+    url(r'^$',views.IndexView.as_view(),name='question_index'),
+    url(r'^(?P<question_id>[0-9]+)/$',views.QuestionView.as_view(),name='question_t_question'),
 ]
