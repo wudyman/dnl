e4eaa02f22912444c7ceff020dab45301bcdcbde
dongxw <wudy.dong@163.com>
2017-10-13 16:45:08 +0800

    add UserProfile add rich text editor

--- question/models.py
+++ question/models.py
@@ -22,10 +22,21 @@ class Topic(models.Model):
 class Answer(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     author=models.ForeignKey(User,on_delete=models.CASCADE)
-    content=models.CharField(max_length=2000)
+    content=models.CharField(max_length=5000)
     pub_date=models.DateTimeField('date published',default=timezone.now)
+    def __str__(self):
+        return self.author.username
 
 class Comment(models.Model):
     author=models.ForeignKey(User,on_delete=models.CASCADE)
     content=models.CharField(max_length=200)
     pub_date=models.DateTimeField('date published',default=timezone.now)
+    def __str__(self):
+        return self.author.username
+
+class UserProfile(models.Model):
+    user=models.OneToOneField(User)
+    phone=models.CharField(default='0',max_length=11)
+    intro=models.CharField(default='brief introduce myself',max_length=200)
+    def __str__(self):
+        return self.user.username
