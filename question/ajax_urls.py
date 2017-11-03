e1516c13da2d2711eae88ff1d1debd73d379573a
dongxw <wudy.dong@163.com>
2017-11-03 18:06:41 +0800

    update

--- question/ajax_urls.py
+++ question/ajax_urls.py
@@ -7,6 +7,7 @@ urlpatterns= [
     url(r'^answer_like/(?P<answer_id>[0-9]+)/$',views.like_answer,name='likeanswer'),
     url(r'^topics/$',views.get_topics,name='gettopics'),
     url(r'^topic_follow/(?P<follow>[0-9]+)/(?P<topic_id>[0-9]+)/$',views.follow_topic,name='followtopic'),
+    url(r'^topic_adept/$',views.get_topic_adept,name='gettopicadept'),
     url(r'^upload/img/$',views.upload_img,name='uploadimage'),
     url(r'^er/(?P<erid>[0-9]+)/$',views.get_erinfo,name='geterinfo'),
     url(r'^er_follow/(?P<follow>[0-9]+)/(?P<er_id>[0-9]+)/$',views.follow_er,name='follow_er'),
