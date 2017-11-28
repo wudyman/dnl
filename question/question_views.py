3176f555a090cf2ab55a1ca97ed2fece497e8fba
dongxw <wudy.dong@163.com>
2017-11-28 18:24:38 +0800

    update

--- question/question_views.py
+++ question/question_views.py
@@ -15,6 +15,8 @@ class IndexView(LoginRequiredMixin,generic.ListView):
     def get_queryset(self):
         pass
     def get(self,request):
+        return render(request,self.template_name,{'user':request.user})
+        '''
         questions=Question.objects.order_by('-pub_date')[0:10]
         question_list=[]
     
@@ -38,6 +40,7 @@ class IndexView(LoginRequiredMixin,generic.ListView):
                     temp.append(str(answer.pub_date)) #12
                     question_list.append(temp)
         return render(request,self.template_name,{'question_list':question_list,'user':request.user})
+        '''
     def post(self,request):
         #print(request.POST.items)
         #print(request.user.__dict__)
@@ -79,7 +82,14 @@ class QuestionView(generic.ListView):
             followed='true'
         else:
             followed='false'
-        return render(request,self.template_name,{'context_question':question,'followed':followed})
+        push_answer_id=request.GET.get('ans')
+        if push_answer_id:
+            print('not none')
+            push_answer=get_object_or_404(Answer,pk=push_answer_id)
+            return render(request,self.template_name,{'user':user,'context_question':question,'followed':followed,'push_answer':push_answer})
+        else:
+            print('none')
+            return render(request,self.template_name,{'user':user,'context_question':question,'followed':followed})
     def post(self,request,*args,**kwargs):
         author=request.user #get_object_or_404(User,username=request.user)
         question_id=self.kwargs.get('question_id')
