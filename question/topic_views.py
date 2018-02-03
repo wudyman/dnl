2375dca7cda18ba2d2db3fc518ab2493f50d3547
dongxw <wudy.dong@163.com>
2018-02-03 19:31:10 +0800

    update

--- question/topic_views.py
+++ question/topic_views.py
@@ -24,20 +24,14 @@ class MyTopicView(LoginRequiredMixin,generic.ListView):
 
 class TopicView(generic.ListView):
     template_name='question/t_topic.html'
-    context_object_name='context_topic'
-    #_question = get_object_or_404(Question,pk=question_id)
     def get_queryset(self):
+        pass
+    def get(self,request,*args,**kwargs):
         topic_id=self.kwargs.get('topic_id')
         topic=get_object_or_404(Topic,pk=topic_id)
-        return topic
-    
-    def post(self,request,*args,**kwargs):
-        author=request.user #get_object_or_404(User,username=request.user)
-        question_id=self.kwargs.get('question_id')
-        question=get_object_or_404(Question,pk=question_id)
-        answer=Answer()
-        answer.content=request.POST.get('content')
-        answer.author=author
-        answer.question=question
-        answer.save()
-        return render(request,self.template_name,{'context_question':question})
+        user=request.user
+        if topic.follower.filter(pk=user.pk).exists():
+            followed='true'
+        else:
+            followed='false'
+        return render(request,self.template_name,{'context_topic':topic,'followed':followed})
