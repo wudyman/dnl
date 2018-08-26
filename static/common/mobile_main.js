321f89450beca0c9c38378ca70455d79ec000b03
dongxw <wudy.dong@yahoo.com>
2018-08-26 11:23:57 +0800

    disable mobile share. change content min charcater number limite.

--- static/common/mobile_main.js
+++ static/common/mobile_main.js
@@ -2879,7 +2879,7 @@ function initElement()
             height:null,
             minHeight:200,
             lang:'zh-CN',
-            placeholder:'请输入文章内容(至少100字)',
+            placeholder:'请输入文章内容(不少于'+TEXT_MIN_LENGTH_HIGH+'个字)',
             disableDragAndDrop:true,
             callbacks: {
                 onImageUpload: function(files){
@@ -3090,8 +3090,8 @@ function initCommon()
     LARGE_TEXT_MAX_LENGTH=10000;
     MIDDLE_TEXT_MAX_LENGTH=500;
     LITTLE_TEXT_MAX_LENGTH=100;
-    TEXT_MIN_LENGTH_HIGH=100;
-    TEXT_MIN_LENGTH_MID=50;
+    TEXT_MIN_LENGTH_HIGH=20;
+    TEXT_MIN_LENGTH_MID=15;
     TEXT_MIN_LENGTH_LOW=10;
     notifications="null";
     messages="null";
@@ -3112,7 +3112,8 @@ function initCommon()
     modify_icon_svg='<svg viewBox="0 0 12 12" class="Icon ModifyButton-icon Icon--modify" width="12" height="16" aria-hidden="true" style="height: 16px; width: 12px;"><title></title><g><path d="M.423 10.32L0 12l1.667-.474 1.55-.44-2.4-2.33-.394 1.564zM10.153.233c-.327-.318-.85-.31-1.17.018l-.793.817 2.49 2.414.792-.814c.318-.328.312-.852-.017-1.17l-1.3-1.263zM3.84 10.536L1.35 8.122l6.265-6.46 2.49 2.414-6.265 6.46z" fill-rule="evenodd"></path></g></svg>';
     like_icon_svg='<svg viewBox="0 0 20 18" xmlns="http://www.w3.org/2000/svg" class="Icon Icon--like Icon--left" width="13" height="16" aria-hidden="true" style="height: 16px; width: 13px;"><title></title><g><path d="M.718 7.024c-.718 0-.718.63-.718.63l.996 9.693c0 .703.718.65.718.65h1.45c.916 0 .847-.65.847-.65V7.793c-.09-.88-.853-.79-.846-.79l-2.446.02zm11.727-.05S13.2 5.396 13.6 2.89C13.765.03 11.55-.6 10.565.53c-1.014 1.232 0 2.056-4.45 5.83C5.336 6.965 5 8.01 5 8.997v6.998c-.016 1.104.49 2 1.99 2h7.586c2.097 0 2.86-1.416 2.86-1.416s2.178-5.402 2.346-5.91c1.047-3.516-1.95-3.704-1.95-3.704l-5.387.007z"></path></g></svg>';
     comment_icon_svg='<svg class="Zi Zi--Comment Button-zi" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M10.241 19.313a.97.97 0 0 0-.77.2 7.908 7.908 0 0 1-3.772 1.482.409.409 0 0 1-.38-.637 5.825 5.825 0 0 0 1.11-2.237.605.605 0 0 0-.227-.59A7.935 7.935 0 0 1 3 11.25C3 6.7 7.03 3 12 3s9 3.7 9 8.25-4.373 9.108-10.759 8.063z" fill-rule="evenodd"></path></svg>'
-    share_button='<div class="Popover ShareMenu ContentItem-action"><div class="" aria-haspopup="true" aria-expanded="false"><img class="ShareMenu-fakeQRCode" src="" alt="微信二维码"><button class="Button Button--plain Button--withIcon Button--withLabel" type="button"><span style="display: inline-flex; align-items: center;">&#8203;<svg class="Zi Zi--Share Button-zi" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M2.931 7.89c-1.067.24-1.275 1.669-.318 2.207l5.277 2.908 8.168-4.776c.25-.127.477.198.273.39L9.05 14.66l.927 5.953c.18 1.084 1.593 1.376 2.182.456l9.644-15.242c.584-.892-.212-2.029-1.234-1.796L2.93 7.89z" fill-rule="evenodd"></path></svg></span>分享</button></div></div>';
+    //share_button='<div class="Popover ShareMenu ContentItem-action"><div class="" aria-haspopup="true" aria-expanded="false"><img class="ShareMenu-fakeQRCode" src="" alt="微信二维码"><button class="Button Button--plain Button--withIcon Button--withLabel" type="button"><span style="display: inline-flex; align-items: center;">&#8203;<svg class="Zi Zi--Share Button-zi" fill="currentColor" viewBox="0 0 24 24" width="1.2em" height="1.2em"><path d="M2.931 7.89c-1.067.24-1.275 1.669-.318 2.207l5.277 2.908 8.168-4.776c.25-.127.477.198.273.39L9.05 14.66l.927 5.953c.18 1.084 1.593 1.376 2.182.456l9.644-15.242c.584-.892-.212-2.029-1.234-1.796L2.93 7.89z" fill-rule="evenodd"></path></svg></span>分享</button></div></div>';
+    share_button='';
     $('#summernote_question').summernote({
         toolbar: [
         // [groupName, [list of button]]
@@ -3156,7 +3157,7 @@ function initCommon()
         ],
         minHeight:150,
         lang:'zh-CN',
-        placeholder:'写回答...',
+        placeholder:'写回答(不少于'+TEXT_MIN_LENGTH_LOW+'个字)',
         callbacks: {
             onImageUpload: function(files){
                 scaleAndUploadImage("forAnswer",files[0],720);
