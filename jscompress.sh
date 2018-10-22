8d0ab24644fd7ea0ca105f9ce9c2aef3e5b15c61
wudy <wudy.dong@gmail.com>
2018-10-22 16:18:45 +0800

    improve code

--- jscompress.sh
+++ jscompress.sh
@@ -11,20 +11,19 @@ else
 fi
 
 java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/main.js -o ./static/common/prod/main.min.${time}.js
-java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/mobile_main.js -o ./static/common/prod/mobile_main.min.${time}.js
+java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/main_mobile.js -o ./static/common/prod/main_mobile.min.${time}.js
 java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/main.css -o ./static/common/prod/main.min.${time}.css
-java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/mobile_main.css -o ./static/common/prod/mobile_main.min.${time}.css
+java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/main_mobile.css -o ./static/common/prod/main_mobile.min.${time}.css
 java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/z_main.css -o ./static/common/prod/z_main.min.${time}.css
-java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/mobile_z_main.css -o ./static/common/prod/mobile_z_main.min.${time}.css
+java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/z_main_mobile.css -o ./static/common/prod/z_main_mobile.min.${time}.css
 
-java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/mobile_business.js -o ./static/common/prod/mobile_business.min.js
 java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/business.js -o ./static/common/prod/business.min.js
+java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/business_mobile.js -o ./static/common/prod/business_mobile.min.js
 java -jar ./yuicompressor-2.4.8.jar ./static/common/dev_staging/citys.js -o ./static/common/prod/citys.min.js
 
 sed -i "s/main.*css/main.min.${time}.css/" ./question/templates/question/head.html
 sed -i "s/main.*js/main.min.${time}.js/" ./question/templates/question/head.html
 
-sed -i "s/mobile_z_main.*css/mobile_z_main.min.${time}.css/" ./question/templates/question/head_mobile.html
-sed -i "s/mobile_main.*css/mobile_main.min.${time}.css/" ./question/templates/question/head_mobile.html
-sed -i "s/mobile_main.*js/mobile_main.min.${time}.js/" ./question/templates/question/head_mobile.html
-
+sed -i "s/main_mobile.*js/main_mobile.min.${time}.js/" ./question/templates/question/head_mobile.html
+sed -i "s/main_mobile.*css/main_mobile.min.${time}.css/" ./question/templates/question/head_mobile.html
+sed -i "s/z_main_mobile.*css/z_main_mobile.min.${time}.css/" ./question/templates/question/head_mobile.html
